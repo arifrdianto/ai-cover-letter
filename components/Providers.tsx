5ef11ca98481b96e55610d71c1c0d778e1d2dbc8
Arif <arifrdianto@gmail.com>
2025-03-28 16:36:51 +0700

    refactor: update ProvidersProps type definition for consistency

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,9 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-interface ProvidersProps {
+type ProvidersProps = {
   children: ReactNode;
-}
+};
 
 export function Providers({ children }: ProvidersProps) {
   return <SessionProvider>{children}</SessionProvider>;
